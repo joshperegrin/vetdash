8ff1f19a3eab3843d6c76dfb37169b71d04b0379
Joshua Peregrin <johnjoshuaperegrin2@gmail.com>
2024-11-30 16:58:46 +0800

    🔍 Added Search/Filter Functionality for STRING
    - Implemented search and filter functionality for string type fields
    - updated ui to cater this feature
    - filters for the following search:
    a. appointmentID
    b. dateTime
    c. createdAt
    d. ownerName
    e. ownerEmail
    f. ownerContact
    g. preferredVet
    h. status
    i. petID
    j. breed
    k. species
    l. dateOfBirth
    m. petName

--- dashboard/js/main.js
+++ dashboard/js/main.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
-import { getFirestore, collection, onSnapshot, query, where, Timestamp } from 'https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js'
+import { getFirestore, collection, onSnapshot, query, where, Timestamp, getDocs, collectionGroup, doc, getDoc, getCountFromServer } from 'https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js'
 
 const firebaseConfig = {
     apiKey: "AIzaSyCn5TPno8hTc1cM-Sm9vsrzkJn6VjKTyYM",
@@ -11,32 +11,198 @@ const firebaseConfig = {
     measurementId: "G-8LEBWB9T8G"
 };
 
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+
 const app = Vue.createApp({
     data() {
         return {
             currentPage: 'appointments', // {overview, appointments, vetSched, settings}
-            appointmentlist: []
+            appointmentlist: [],
+            appointmentSearchTerm: "",
+            appointmentSearchFilter: "appointmentID",
         }
     },
 
     async mounted() {
         try {
-            const firebaseApp = initializeApp(firebaseConfig);
-            const db = getFirestore(firebaseApp);
-
-            const q = query(
-                collection(db, "appointments"),
-                where('dateTime', '>=', Timestamp.fromDate(new Date(2024, 10, 17))),
-                where('dateTime', '<', Timestamp.fromDate(new Date(2024, 10, 18)))
-            )
-
-            let snap = onSnapshot(q, (snapshot) => { // map collections array to this.appointmentss
-                this.appointmentlist = snapshot.docs.map((doc) => doc.data())
-            })
 
         } catch (e) {
             console.log(e)
         }
+    },
+
+    methods: {
+        async fetchQuery() {
+            
+            if (this.appointmentSearchTerm == "") {
+                return
+            }
+            try {
+                /**
+                 * (Date) createdAt - date of booking of the appointment
+                 * (Date) dateTime - schedule of the appointment
+                 * (String) otherConcerns
+                 * (String) ownerAddress
+                 * (String) ownerContact
+                 * (String) ownerEmail
+                 * (String) ownerName
+                 * (String) preferredVet
+                 * (String) status
+                 * (Date) updatedAt
+                 * (String) visitReason
+                 * 
+                 * (String) breed
+                 * (Date) dateOfBirth
+                 * (Boolean) gender [true = male, false = female]
+                 * (String) petName
+                 * (String) species
+                 */
+
+                let q;
+                
+                switch (this.appointmentSearchFilter) {
+                    case "appointmentID":
+                        debugger;
+                        q = query(
+                            collectionGroup(db, "appointments"),
+                            where('appointmentID', '>=', this.appointmentSearchTerm),
+                            where('appointmentID', '<=', this.appointmentSearchTerm + '\uf8ff')
+                        );
+                        break;
+                    case "createdAt":
+                        throw "Error: Not Implemented Yet."
+                        return;
+                        break;
+                    case "dateTime":
+                        throw "Error: Not Implemented Yet."
+                        return;
+                        break;
+                    // case "otherConcerns":
+                    //     break;
+                    // case "ownerAddress":
+                    //     break;
+                    case "ownerContact":
+                        q = query(
+                            collectionGroup(db, "appointments"),
+                            where('ownerContact', '>=', this.appointmentSearchTerm),
+                            where('ownerContact', '<=', this.appointmentSearchTerm + '\uf8ff')
+                        );
+                        break;
+                    case "ownerEmail":
+                        q = query(
+                            collectionGroup(db, "appointments"),
+                            where('ownerEmail', '>=', this.appointmentSearchTerm),
+                            where('ownerEmail', '<=', this.appointmentSearchTerm + '\uf8ff')
+                        );
+                        break;
+                    case "ownerName":
+                        q = query(
+                            collectionGroup(db, "appointments"),
+                            where('ownerName', '>=', this.appointmentSearchTerm),
+                            where('ownerName', '<=', this.appointmentSearchTerm + '\uf8ff')
+                        );
+                        break;
+                    case "petID":
+                        const collectionSnapshot = await getDocs(collection(db, "pets", this.appointmentSearchTerm, "appointments"));
+                        this.appointmentlist = collectionSnapshot.docs.map(doc => doc.data());
+                        console.log(this.appointmentlist)
+                        return;
+                        break;
+                    case "preferredVet":
+                        q = query(
+                            collectionGroup(db, "appointments"),
+                            where('preferredVet', '>=', this.appointmentSearchTerm),
+                            where('preferredVet', '<=', this.appointmentSearchTerm + '\uf8ff')
+                        );
+                        break;
+                    case "status": // options pills
+                        q = query(
+                            collectionGroup(db, "appointments"),
+                            where('status', '==', this.appointmentSearchTerm)
+                        );
+                        break;
+                    // case "updatedAt":
+                    //     break;T
+                    // case "visitReason":
+                    //     break;
+                    case "breed":
+                        q = query(
+                            collection(db, "pets"),
+                            where('breed', '>=', this.appointmentSearchTerm),
+                            where('breed', '<=', this.appointmentSearchTerm + '\uf8ff')
+                        );
+
+                        break;
+                    case "dateOfBirth":
+                        throw "Error: Not Implemented Yet."
+                        return;
+                        break;
+                    // case "gender":
+                    //     break;
+                    case "petName": 
+                        q = query(
+                            collection(db, "pets"),
+                            where('petName', '>=', this.appointmentSearchTerm),
+                            where('petName', '<=', this.appointmentSearchTerm + '\uf8ff')
+
+                        );
+                        break;
+                    case "species":
+                        q = query(
+                            collection(db, "pets"),
+                            where('species', '>=', this.appointmentSearchTerm),
+                            where('species', '<=', this.appointmentSearchTerm + '\uf8ff')
+                        );
+                        break;
+
+                    default:
+                        throw "Skill Issue: No filter Selected"
+                }
+
+
+
+                if (this.appointmentSearchFilter == "breed"
+                    || this.appointmentSearchFilter == "dateOfBirth"
+                    || this.appointmentSearchFilter == "petName"
+                    || this.appointmentSearchFilter == "species") {
+                    
+                    const querySnapshot = await getDocs(q);
+
+                    let rowsCount = 0;
+                    const pageLimit = 12;
+                    this.appointmentlist = []
+                    await Promise.all(querySnapshot.docs.forEach(async (element) => {
+                        const appointmentsCollection = collection(db, "pets", element.id, "appointments");
+                        const countSnapshot = await getCountFromServer(appointmentsCollection);
+
+                        if (countSnapshot.data().count + rowsCount <= pageLimit) {
+                            q = await getDocs(appointmentsCollection);
+                            this.appointmentlist.push(...q.docs.map(doc => doc.data()))
+                        } else if (rowsCount < pageLimit) {
+                            q = query(appointmentsCollection, orderBy("dateTime"), limit(pageLimit - rowsCount));
+                            const querySnapshot2 = await getDocs(q);
+                            this.appointmentlist.push(...querySnapshot2.docs.map(doc => doc.data()));
+                        }
+                        rowsCount += countSnapshot.data().count;
+                        debugger;
+
+                    }))
+                    
+                } else {
+                    debugger;
+                    const querySnapshot = await getDocs(q);
+                    this.appointmentlist = querySnapshot.docs.map(doc => doc.data())
+                }
+
+                
+            } catch (e) {
+                console.error(e)
+            }
+
+        }
+
+
     }
 })
 
